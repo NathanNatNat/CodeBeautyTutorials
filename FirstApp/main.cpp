c7fbec8ba6cfe542cf662417149b1e860e9f2c19
natnat <33712044+natnat201193@users.noreply.github.com>
2021-01-26 12:54:45 +0000

    FirstAppMain

---
+++ FirstApp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "My name is Nathan." << std::endl;
+    std::cout << "today is Tuesday.\n";
+
+
+    system("pause>0");
+}
