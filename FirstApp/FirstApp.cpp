074e02bd96518d3df5cbbeb2cef399dfa999fd57
natnat <33712044+natnat201193@users.noreply.github.com>
2021-01-26 12:14:01 +0000

    test

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -17,4 +17,4 @@ int main()
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file 
