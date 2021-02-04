01bbfeae8b508706606914b243a1834f1d2f82c4
natnat <33712044+natnat201193@users.noreply.github.com>
2021-02-04 03:38:05 +0000

    05:44:30​ – Introduction to functions

---
+++ FunctionsIntro/FunctionsIntro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void function();
+
+int main()
+{
+	cout << "Hello from main()\n";
+	function();
+
+	system("pause>0");
+}
+
+void function()
+{
+	cout << "hello from function()\n";
+}
